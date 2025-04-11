35bd86e0868ac45b6fa4418eabb3bc20f3929bd6
BoomStoneCraft <boom.stone.craft@gmail.com>
2025-04-11 09:16:27 -0600

    JS maybe

---
+++ uhgod.js
@@ -0,0 +1,28 @@
+function getlikes () {
+    fetch("./Ulike.txt")
+        .then((res) => res.text())
+        .then((text) => {
+            console.log(text)
+        })
+        
+    
+}
+
+
+
+function Ulike () {
+    fetch("./Ulike.txt")
+        .then((res) => res.text())
+        .then((text) => {
+            var f = "Ulike.txt";
+            writeTextFile(f, text+1)
+
+            function writeTextFile(afilename, output)
+            {
+                var txtfile = new File(afilename);
+                txtfile.writeln(output);
+                txtfile.close();
+            }
+        })
+
+}
